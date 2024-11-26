4b56e4f6a61f0f68749a298bccf1cb19eab4f6ff
Huskarnov <youcef.merad.batna2m@gmail.com>
2024-11-26 11:12:37 +0100

    final

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// const right = document.querySelector(".main-right");
+// const left = document.querySelector(".main-left");
+// const cardList = document.querySelector(".card-list");
+
+
+// const rightHeight = right.offsetHeight ;
+
+// cardList.style.height = (rightHeight - 15 - 40  - 24 ) + 'px';
+
+// console.log(rightHeight);
